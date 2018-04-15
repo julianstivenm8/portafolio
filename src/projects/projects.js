655c5944ca69242958f66d97caceba7b897e8f58
Julian Madrid <julianstivenm8@gmail.com>
2018-04-15 13:22:21 -0500

    projectRoutes

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -2,8 +2,41 @@ import React, { Component } from 'react';
 import '../App.css';
 import Lsvg from '../icons/components/Lsvg.js';
 import Header from '../header/header.js'
+import { Route, Link  } from 'react-router-dom'
 
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+);
 class Projects extends Component {
+
+Topico(){
+  const Topics = ({ match }) => (
+    <div>
+    <div className='hijoTresDos '>
+      <h2 className='projdesc'>Process Step</h2>
+      <div className='projdesc'>
+     <Link to={`${match.url}/rendering`}>  <h3>1</h3></Link>
+     <Link to={`${match.url}/rendering`}>  <h3 className='projSpace'>2</h3></Link>
+     <Link to={`${match.url}/rendering`}>  <h3 className='projSpace'>3</h3></Link>
+     <Link to={`${match.url}/rendering`}>  <h3 className='projSpace'>Final</h3></Link>
+      </div>
+    </div>
+    <div className='hijoTresTres'>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et massa sem. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed non placerat metus. Suspendisse fermentum eu est ac ullamcorper. Quisque porta tincidunt nibh, eu sagittis leo lacinia id. Duis egestas, augue sit amet venenatis.
+      </p>
+    </div>
+    <Route path={`${match.url}/:topicId`} component={Topic} />
+    <Route
+      exact
+      path={match.url}
+      render={() => <h3>Please select a topic.</h3>}
+    />
+    </div>
+);
+}
 
   render() {
     return (
@@ -27,7 +60,8 @@ class Projects extends Component {
            <h3 className='projSpace'>2017</h3>
            </div>
          </div>
-         <div className='hijoTresDos '>
+         {this.Topico()}
+         {/* <div className='hijoTresDos '>
            <h2 className='projdesc'>Process Step</h2>
            <div className='projdesc'>
            <h3>1</h3>
@@ -37,10 +71,11 @@ class Projects extends Component {
            </div>
          </div>
          <div className='hijoTresTres '>
+           {this.Topico()}
            <p>
              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et massa sem. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed non placerat metus. Suspendisse fermentum eu est ac ullamcorper. Quisque porta tincidunt nibh, eu sagittis leo lacinia id. Duis egestas, augue sit amet venenatis.
            </p>
-         </div>
+         </div> */}
          <div className='hijoTresCuatro '>
            <h2 className='projdesc'>Repositorio o url del proyecto:</h2>
            <div className='projdesc'>
